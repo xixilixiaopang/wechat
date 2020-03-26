5157bf46cad286e039abeb8896bb7a8e60fa9ed3
郑少帅 <zhengshaoshuai@highershine.com>
2020-03-26 15:58:11 +0800

    '小程序项目创建第一次提交'

---
+++ wechat-interfaces/src/test/java/com/shuai/wechat/interfaces/WechatInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuai.wechat.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatInterfacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
