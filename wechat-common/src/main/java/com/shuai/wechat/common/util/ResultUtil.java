1447b5ec567ffc39bb18cd4bfb9f0df4f027ed1d
zhengshaoshuai@highershine.com <zhengshaoshuai@highershine.com>
2020-07-01 16:49:59 +0800

    基础模块整理提交

--- wechat-common/src/main/java/com/shuai/wechat/common/util/ResultUtil.java
+++ wechat-common/src/main/java/com/shuai/wechat/common/util/ResultUtil.java
@@ -1,19 +1,175 @@
 package com.shuai.wechat.common.util;
 
+
+import com.shuai.wechat.common.entity.result.ExceptionEnum;
 import com.shuai.wechat.common.entity.result.Result;
+import com.shuai.wechat.common.entity.result.ResultEnum;
 import com.shuai.wechat.common.enums.HttpStatusEnum;
 
 /**
- * @Description: TODO
- * @Author: zhengshaoshuai
- * @Date: 2020/3/27 11:27
+ * @author highershine.mizhanlei
+ * @date 2018/8/27 13:03
  */
 public final class ResultUtil {
+    private ResultUtil() {
 
+    }
+    /**
+     * 成功
+     *
+     * @return
+     */
+    public static Result successResult() {
+        Result result = new Result();
+        result.setCode(200);
+        result.setMsg("成功");
+        return result;
+    }
+
+    /**
+     * 成功
+     *
+     * @param object
+     * @return
+     */
+    public static Result successResult(Object object) {
+        Result result = new Result();
+        result.setCode(200);
+        result.setData(object);
+        result.setMsg("成功");
+
+        return result;
+    }
+
+    /**
+     * 成功
+     *
+     * @param resultEnum
+     * @return
+     */
+    public static Result successResult(ResultEnum resultEnum) {
+        Result result = new Result();
+        result.setCode(resultEnum.getCode());
+        result.setMsg(resultEnum.getMsg());
+
+        return result;
+    }
+
+    /**
+     * 成功
+     *
+     * @param httpStatusEnum
+     * @return
+     */
     public static Result successResult(HttpStatusEnum httpStatusEnum) {
         Result result = new Result();
         result.setCode(httpStatusEnum.getCode());
         result.setMsg(httpStatusEnum.getMsg());
+
+        return result;
+    }
+
+    /**
+     * 成功
+     *
+     * @param httpStatusEnum
+     * @return
+     */
+    public static Result successResult(HttpStatusEnum httpStatusEnum, Object object) {
+        Result result = new Result();
+        result.setCode(httpStatusEnum.getCode());
+        result.setMsg(httpStatusEnum.getMsg());
+        result.setData(object);
+
+        return result;
+    }
+
+    /**
+     * 成功
+     *
+     * @param resultEnum
+     * @param object
+     * @return
+     */
+    public static Result successResult(ResultEnum resultEnum, Object object) {
+        Result result = new Result();
+        result.setCode(resultEnum.getCode());
+        result.setMsg(resultEnum.getMsg());
+        result.setData(object);
+
+        return result;
+    }
+
+    /**
+     * 成功
+     *
+     * @param code
+     * @param msg
+     * @param object
+     * @return
+     */
+    public static Result successResult(Integer code, String msg, Object object) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setData(object);
+        return result;
+    }
+
+    /**
+     * 成功
+     *
+     * @param code
+     * @param msg
+     * @return
+     */
+    public static Result successResult(Integer code, String msg) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
+
+
+    /**
+     * 失败
+     *
+     * @param code
+     * @param msg
+     * @return
+     */
+    public static Result errorResult(Integer code, String msg) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
+
+    /**
+     * 失败
+     *
+     * @param resultEnum
+     * @return
+     */
+    public static Result errorResult(ResultEnum resultEnum) {
+        Result result = new Result();
+        result.setCode(resultEnum.getCode());
+        result.setMsg(resultEnum.getMsg());
+
+        return result;
+    }
+
+    /**
+     * 失败
+     *
+     * @param exceptionEnum
+     * @return
+     */
+    public static Result errorResult(ExceptionEnum exceptionEnum) {
+        Result result = new Result();
+        result.setCode(exceptionEnum.getCode());
+        result.setMsg(exceptionEnum.getMsg());
+
         return result;
     }
 }
--- wechat-system/src/main/java/com/shuai/wechat/system/WechatSystemApplication.java
+++ wechat-system/src/main/java/com/shuai/wechat/system/WechatSystemApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @MapperScan("com.shuai.wechat.*.mapper")
-@ComponentScan(basePackages = {"com.shuai.wechat.common.*", "com.shuai.wechat.system.*"} )
+@ComponentScan(basePackages = {"com.shuai.wechat.common", "com.shuai.wechat.system"} )
+@EnableAsync
 public class WechatSystemApplication {
 
     public static void main(String[] args) {
